4856cd66c84651bf41135ab550db4dc3717dde98
kamycoding <kamy.codes@gmail.com>
2026-05-24 15:09:50 +0200

    feat: connect like button with book index

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 const renderBooks = () => {
   const bookList = document.getElementById("bookList");
 
-  const booksHtml = books.map((book) => getBookTemplate(book)).join("");
+  const booksHtml = books
+    .map((book, index) => getBookTemplate(book, index))
+    .join("");
 
   bookList.innerHTML = booksHtml;
+};
+
+const toggleLike = (index) => {
+  console.log(index);
 };
 
 renderBooks();
